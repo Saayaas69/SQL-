59ea300f31bd77048760432af3483fb08af19fb0
Saayaas <122041700+Saayaas69@users.noreply.github.com>
2025-01-28 17:28:31 +0545

    deleting set 1 folder for now

--- Set 1/SchemaDefinition.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Students (
-  StudentID INT PRIMARY KEY AUTO_INCREMENT,
-  FirstName VARCHAR(50) NOT NULL,
-  LastName VARCHAR(50) NOT NULL,
-  Age INT CHECK (Age >= 16),
-  EnrollmentDate DATE DEFAULT CURRENT_DATE,
-  Major VARCHAR(100)
-);
